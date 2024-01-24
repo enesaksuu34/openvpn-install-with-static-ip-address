0b7936589f0286b7fcf8ab6b850c01764395b085
Enes Aksu <88115318+enesaksuu34@users.noreply.github.com>
2024-01-25 02:38:51 +0300

    update

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -498,7 +498,21 @@ case "$option" in
 			cd /etc/openvpn/server/easy-rsa/
 			./easyrsa --batch --days=3650 build-client-full "$client" nopass
 			# Generates the custom client.ovpn
-			new_client
+			{
+			cat /etc/openvpn/server/client-common.txt
+			echo "<ca>"
+			cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+			echo "</ca>"
+			echo "<cert>"
+			sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt
+			echo "</cert>"
+			echo "<key>"
+			cat /etc/openvpn/server/easy-rsa/pki/private/"$client".key
+			echo "</key>"
+			echo "<tls-crypt>"
+			sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
+			echo "</tls-crypt>"
+			} > ~/"$client".ovpn
 			echo
 			echo "$client added. Configuration available in:" ~/"$client.ovpn"
 			exit
